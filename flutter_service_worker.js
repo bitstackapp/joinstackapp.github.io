07a9446f378bc8bd387d01c6bc2bdc7a40cf7f0a
joinstackapp <kabir.sethi2@mail.mcgill.ca>
2021-03-15 23:00:22 +0000

    See https://github.com/joinstackapp/website_teaser/commit/60c7fada52e10351b30691f54ee0e2344208db60

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,9 +29,9 @@ const RESOURCES = {
 "assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "d80ca32233940ebadc5ae5372ccd67f9",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "831eb40a2d76095849ba4aecd4340f19",
 "assets/FontManifest.json": "4cedb49bd09d72bc043ab3f8dc199d4c",
-"index.html": "122c654564895de52d7b6c53a964b467",
-"/": "122c654564895de52d7b6c53a964b467",
-"main.dart.js": "3aea9a35adfe04df68b61c3d9028f68f",
+"index.html": "412d059b2de96202c080ae243c29b5ac",
+"/": "412d059b2de96202c080ae243c29b5ac",
+"main.dart.js": "97f069bf899aa87b35617356ba04298a",
 "CNAME": "50cc6866259c5da1ce5fabc0be2ab16f",
 "version.json": "b10b7790f67f9b8baf201ae1133c1fe3",
 "favicon.png": "115b79792c869e3a15803b10c06ed6ad"
